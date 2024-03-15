d091bc15199f0b28fbabbd5ad7405bfd3b6d5010
Syahmi Azril Sobri <syahmi1908azril@gmail.com>
2024-03-15 15:04:15 +0800

    Added routing and navbar

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,5 @@
+const Shop = () => {
+    return <h1>THIS IS THE SHOP PAGE</h1>
+}
+
+export default Shop;
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,11 @@
+const SignIn = () => {
+    return (
+        <div>
+            <h1>
+                THIS IS THE SIGN IN PAGE
+            </h1>
+        </div>
+    )
+}
+
+export default SignIn;
